01d59f8fdf1f07c57fa91d6b0d19c5489b7c7d64
jasineri <jasineri@gmx.de>
2018-01-18 12:20:26 +0100

    Change package

--- src/main/java/de/jasineri/common/util/Optional.java
+++ src/main/java/de/jasineri/common/util/Optional.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package common.util;
+package de.jasineri.common.util;
 
 
 import java.util.NoSuchElementException;
